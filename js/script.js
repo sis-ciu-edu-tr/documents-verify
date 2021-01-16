8d33e2110e9ed2093e531ba5b8b9ccc0fc18b2da
CIU <ochamur@gmail.com>
2021-01-16 06:13:27 +0200

    asddsa

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-var pass = prompt("Enter the Verification Code:", "");
-if (pass == null)  window.location = "../wrong.html";
-else if (pass == "5Ws14Wqa")  
-      window.location = "../index.html";
-else  window.location = "../wrong.html";
+// var pass = prompt("Enter the Verification Code:", "");
+// if (pass == null)  window.location = "../wrong.html";
+// else if (pass == "5Ws14Wqa")  
+//       window.location = "../index.html";
+// else  window.location = "../wrong.html";
+
